3e738cfcb4a2d9194cc300ebac2bf48c306d27c1
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2014-02-18 12:40:41 -0500

    Initial check-in

---
+++ websurv/settings/base.py
@@ -0,0 +1,39 @@
+from django.contrib import messages
+
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(3)
+
+SECRET_KEY = 'ea+sg5filpj)308_9t2tzuoay6u=p1cdmqt!!s@=tfsunoybf$'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'websurv.urls'
+WSGI_APPLICATION = 'websurv.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'EST'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+MESSAGE_TAGS = {
+    messages.constants.ERROR: 'danger'    # Fix up for Bootstrap.
+}
---
+++ websurv/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': PROJECT_DIR.child("db.sqlite3")
+    }
+}
+
+INSTALLED_APPS += ('debug_toolbar',)
