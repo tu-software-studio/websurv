c8c921d90d407583dd6b4ad12112b0fa11351f3b
dustinwaldron <dustin_waldron@taylor.edu>
2014-02-21 16:19:28 -0500

    modified base.html and urls and view files

--- websurv/urls.py
+++ websurv/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from thin.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'websurv.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^$', home),
     url(r'^admin/', include(admin.site.urls)),
 )
