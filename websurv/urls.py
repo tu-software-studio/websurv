36bd86481edc863258d8a3944af4012a8987e9e9
Adam Keenan <adam_keenan@taylor.edu>
2014-02-21 22:13:35 -0500

    Finished up work on base.html

--- websurv/urls.py
+++ websurv/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'websurv.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
-    url(r'^$', home),
+
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
