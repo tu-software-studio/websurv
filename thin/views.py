4d63438ca3b879bebe8ab4d1d39dde4b60a234ae
Josh Wilhelm <wilhelm.josh@gmail.com>
2014-02-24 21:10:36 -0500

    Started on survey view

--- thin/views.py
+++ thin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404
-from backend.models import Dictionary
+from backend.models import Dictionary, Survey, Variety
 
 # Create your views here.
 def home(request):
@@ -8,4 +8,9 @@ def home(request):
 def dictionary_index(request):
     dictionaries = get_list_or_404(Dictionary)
     return render(request, 'thin/dictionary_index.html', {'d_lst': dictionaries})
+
+def survey_index(request):
+    surveys_list = get_list_or_404(Variety, Variety.name != null)
+    context = {'surveys_list': surveys_list}
+    return render(request, 'survey_index.html', context)
 
--- websurv/urls.py
+++ websurv/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^dictionaries/$', dictionary_index, name='dictionary_index')
+    url(r'^dictionaries/$', dictionary_index, name='dictionary_index'),
+    url(r'^surveys/$', survey_index, name='survey_index')
 )
