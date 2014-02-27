5a75fce46f5d5112a97dfb242041a9c8761d0b54
Justin Southworth <justin_southworth@taylor.edu>
2014-02-27 15:51:55 -0500

    Started tests.

--- thin/tests.py
+++ thin/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test.client import Client
+
+from backend.models import Dictionary, Project
 
 # Create your tests here.
+class DictionaryIndexTestCase(TestCase):
+    def set_up(self):
+        Project.objects.create(name="project_1")
+        Dictionary.objects.create(name="dictionary_1", project="project_1")
+
+    def test_index_contains_dictionary(self):
+        client = Client()
+        response = client.get('dictionary_index')
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['dictionaries']), 1)
