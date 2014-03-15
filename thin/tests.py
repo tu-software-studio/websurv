c1e2f22d8e1b5a4e0bd2f6a12a7f5ab4db1ba087
Justin Southworth <justin_southworth@taylor.edu>
2014-03-15 17:52:26 -0400

    Added exists tests for everything but comparisons.

--- thin/tests.py
+++ thin/tests.py
@@ -1,10 +1,23 @@
+from django.db import models
 from django.test import TestCase
 from django.test.client import Client
 from django.utils import unittest
 
-from backend.models import Dictionary, Project
+from backend.models import Dictionary, Gloss, PartOfSpeech, Project, Survey, Variety
 
-# Create your tests here.
+class SessionsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_sessions_login_exists(self):
+        response = self.client.get('/login/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_sessions_logout_exists(self):
+        response = self.client.get('/logout/')
+        self.assertEquals(response.status_code, 302)
+
+        
 class DictionaryTestCase(TestCase):
     def setUp(self):
         self.client = Client()
@@ -41,34 +54,140 @@ class DictionaryTestCase(TestCase):
         self.assertEqual(len(response.context['dictionary_list']), 2)
 
 
+class GlossTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.pos = PartOfSpeech.objects.create(name='noun')
+        self.project = Project.objects.create(name="project_1")
+        self.dictionary = Dictionary.objects.create(name='dictionary_1', project=self.project)
+        self.instance = Gloss.objects.create(
+            primary="primary_1",
+            secondary="secondary_1",
+            pos=self.pos,
+            dictionary=self.dictionary,
+            field_tip="field_tip_1",
+            comment_tip="comment_tip_1",
+            created_at = models.DateTimeField(auto_now_add=True)
+        )
+
+    def test_gloss_add_exists(self):
+        response = self.client.get('/glosses/add/' + str(self.dictionary.id) + '/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_gloss_delete_exists(self):
+        response = self.client.post('/glosses/' + str(self.instance.id) + '/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_gloss_detail_exists(self):
+        response = self.client.get('/glosses/' + str(self.instance.id) + '/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_gloss_edit_exists(self):
+        response = self.client.get('/glosses/' + str(self.instance.id) + '/edit/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_gloss_index_exists(self):
+        response = self.client.get('/glosses/')
+        self.assertEqual(response.status_code, 200)
+
+        
 class ProjectTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.instance = Project.objects.create(name="test")
+        self.instance = Project.objects.create(name="project_1")
 
-    def test_project_page_index_works(self):
-        response = self.client.get('/projects/')
+    def test_project_add_exists(self):
+        response = self.client.post('/projects/add/')
         self.assertEqual(response.status_code, 200)
-        
+
     def test_project_add(self):
         self.client.post('/projects/add/', {'name' : 'minombre'})
         response = self.client.get('/projects/2/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,'minombre')
               
-    def test_project_index_has_the_project(self):
-        response = self.client.get('/projects/')
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(len(response.context['project_list']),1)
-
     def test_project_delete_works(self):
         self.client.post('/projects/1/delete/')
         response = self.client.get('/projects/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['project_list']),0)
 
-    def test_edit_project_name(self):
-        self.client.post('/projects/1/edit/', {'name' : 'changedit'})
-        response = self.client.get('/projects/1/')
+    def test_project_detail_exists(self):
+        response = self.client.post('/projects/' + str(self.instance.id) + '/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response,'changedit')
+
+    def test_project_edit_exists(self):
+        response = self.client.post('/projects/' + str(self.instance.id) + '/edit/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_project_name(self):
+        self.client.post('/projects/1/edit/', {'name' : 'new_name'})
+        response = self.client.get('/projects/1/')
+        self.assertContains(response, 'new_name')
+
+    def test_project_index_exists(self):
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_project_index_contains_project(self):
+        Project.objects.create(name='project_2')
+        response = self.client.get('/projects/')
+        self.assertEqual(len(response.context['project_list']), 2)
+
+
+class SurveyTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.project = Project.objects.create(name="project")
+        self.dictionary = Dictionary.objects.create(name="dictionary_1", project=self.project)
+        self.instance = Survey.objects.create(name="survey_1", title="Title", dictionary=self.dictionary)
+
+    def test_survey_add_exists(self):
+        response = self.client.get('/surveys/add/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_survey_delete_exists(self):
+        response = self.client.post('/surveys/' + str(self.instance.id) + '/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_survey_detail_exists(self):
+        response = self.client.get('/surveys/' + str(self.instance.id) + '/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_survey_edit_exists(self):
+        response = self.client.get('/surveys/' + str(self.instance.id) + '/edit/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_survey_index_exists(self):
+        response = self.client.get('/surveys/')
+        self.assertEqual(response.status_code, 200)
+
+
+class VarietyTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.project = Project.objects.create(name="project")
+        self.dictionary = Dictionary.objects.create(name="dictionary_1", project=self.project)
+        self.survey = Survey.objects.create(name="survey_1", title="Title", dictionary=self.dictionary)
+        self.instance = Variety.objects.create(name='variety_1', survey=self.survey)
+
+    def test_variety_add_exists(self):
+        response = self.client.get('/varieties/add/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_variety_delete_exists(self):
+        response = self.client.post('/varieties/' + str(self.instance.id) + '/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_variety_detail_exists(self):
+        response = self.client.get('/varieties/' + str(self.instance.id) + '/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_variety_edit_exists(self):
+        response = self.client.get('/varieties/' + str(self.instance.id) + '/edit/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_variety_index_exists(self):
+        response = self.client.get('/varieties/')
+        self.assertEqual(response.status_code, 200)
+
