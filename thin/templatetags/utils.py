d8652562c7f5b164eb19ab4edfbca898abd86f95
Adam Keenan <adam_keenan@taylor.edu>
2014-03-15 02:42:15 -0400

    Added active class to navbar

--- thin/templatetags/utils.py
+++ thin/templatetags/utils.py
@@ -1,7 +1,8 @@
 from django import template
+
 register = template.Library()
+
 
 @register.simple_tag
 def blink_to(url, text="BSButtonLink", btype="btn-default"):
     return "<a href='{}' class='btn {}'>{}</a>".format(url, btype, text)
-
